aa5c8a00d27e57bec7d7862a225c1ac0c1d60ded
Ataullah Atal <atalirfan@gmail.com>
2022-11-09 17:08:30 -0500

    this is for remote

--- src/gitPractice/firstClassGit.java
+++ src/gitPractice/firstClassGit.java
@@ -5,6 +5,7 @@ public class firstClassGit {
 	public static void main(String[] args) {
 		System.out.println("this is test");
 		System.out.println("i am atal");
+		System.out.println("atiq is my friend");
 
 	}
 
